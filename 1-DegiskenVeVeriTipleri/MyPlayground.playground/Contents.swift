9b3a837161230eda3fa8cc973a42efcc58877c85
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-22 22:19:36 +0300

    feat(playground): add initial playground setup with variables
    * Introduced student and product variables for demonstration
    * Included print statements to output variable values

---
+++ 1-DegiskenVeVeriTipleri/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var studentName = "Musa"
+var studentAge = 26
+var studentTall = 1.78
+
+
+print(studentName)
+print(studentAge)
+print(studentTall)
+
+
+var productId : Int = 3416
+var productName : String = "Kol saati"
+var productQuantity : Int = 100
+var productPrice : Double = 149.99
+var productProvider : String = "Rolex"
+
+print(productId)
+print(productName)
+print(productQuantity)
+print(productPrice)
+print(productProvider)
