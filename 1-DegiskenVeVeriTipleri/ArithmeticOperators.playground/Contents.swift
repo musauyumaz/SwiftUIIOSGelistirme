d9dc8328c344c4964f012688e0d9acf25aefc762
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-24 16:59:23 +0300

    feat(arithmetic-operators): add playground for arithmetic operations
    * Implement basic arithmetic operations and formulas
    * Include calculations for area, force, and motion

---
+++ 1-DegiskenVeVeriTipleri/ArithmeticOperators.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+//Daire alani
+
+let pi = 3.14
+
+let yaricap = 2.0
+
+let alan = pi * yaricap * yaricap
+
+print("Daire Alanı: \(alan)")
+
+//F = m * a
+
+let m = 12.5
+let a = 10.0
+let f = m * a
+print(f)
+
+//Ax
+var v = 12.7
+var v0 = 23.56
+var t = 3.5
+var a1 = ((v+v0)/2)*t
+print(a1)
+
+
+var x2 = (v0 * t) + (a * t * t) / 2
+print(x2)
+
+
+var y = 10
+
+y = y + 2
+
+print(y)
+
+y+=2
+print(y)
+
+
+y -= 3
+print(y)
+
+y*=4
+print(y)
+
+y/=2
+print(y)
+
+
+
