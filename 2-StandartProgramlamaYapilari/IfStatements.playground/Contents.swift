6e2abf86c4d5f3b891118ea4e621c4c148cdbf74
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-25 22:43:11 +0300

    feat(IfStatements): add basic arithmetic operations based on user choice
    * Introduced a simple menu for addition, subtraction, multiplication, and division
    * Implemented conditional statements to execute the chosen operation

--- 2-StandartProgramlamaYapilari/IfStatements.playground/Contents.swift
+++ 2-StandartProgramlamaYapilari/IfStatements.playground/Contents.swift
@@ -81,3 +81,26 @@ if secim == 2 {
     print("Yarıçap : \(yariCap)")
     print("Sonuç : \(3.14 * Double(yariCap) * Double(yariCap))")
 }
+
+//Örnek
+print("Toplama(1)")
+print("Çıkarma(2)")
+print("Çarpma(3)")
+print("Bölme(4)")
+
+let tercih = 2
+let sayi1 = 100
+let sayi2 = 20
+
+print("Tercihiniz : \(tercih)")
+
+if tercih == 1{
+    print("Toplama : \(sayi1 + sayi2)")
+} else if tercih == 2{
+    print("Çıkarma : \(sayi1 - sayi2)")
+}else if tercih == 3{
+    print("Çarpma : \(sayi1 * sayi2)")
+}else if tercih == 4{
+    print("Bölme : \(sayi1 / sayi2)")
+}
+
