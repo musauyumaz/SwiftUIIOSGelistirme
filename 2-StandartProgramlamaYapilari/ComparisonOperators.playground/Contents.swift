8bfc53931be28f3c4855f765dd18cd2698e47600
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-25 21:41:26 +0300

    feat(playground): add comparison operators example
    * Implemented basic comparison operators in Swift
    * Created playground file for interactive testing

---
+++ 2-StandartProgramlamaYapilari/ComparisonOperators.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let s1 = 60
+let s2 = 50
+
+let y1 = 70
+let y2 = 80
+
+print(s1 == s2)
+print(s1 > s2)
+print(s1 >= s2)
+print(s1 < s2)
+print(s1 <= s2)
+print(s1 != s2)
+
+print(s1 > s2 || y1 > y2)
+
+print(s1 > s2 && y1 > y2)
+
