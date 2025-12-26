58953492d436bc3246101092a4cdabcb6a6fb870
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-26 23:55:03 +0300

    feat(optional): add optional binding example in playground
    * Introduced an example of using optional binding with if let
    * Demonstrated handling nil values in Swift

--- 2-StandartProgramlamaYapilari/Optional.playground/Contents.swift
+++ 2-StandartProgramlamaYapilari/Optional.playground/Contents.swift
@@ -8,3 +8,15 @@ if str != nil{
 }else{
     print("str nil değer içeriyor")
 }
+
+// if let
+
+var str2 : String?
+str2 = "Merhaba"
+
+if let temp = str2{
+    print(temp)
+}else{
+    print("str2 nil değer içeriyor")
+}
+
