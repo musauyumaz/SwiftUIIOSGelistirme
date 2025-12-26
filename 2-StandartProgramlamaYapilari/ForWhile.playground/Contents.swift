c120e78c4c5606d4a9ad37e2a035d91c73de1f21
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-12-26 17:23:19 +0300

    feat(playground): add ForWhile playground with loop examples
    * Implemented for and while loops demonstrating basic iteration
    * Created playground structure for easy experimentation with Swift code

---
+++ 2-StandartProgramlamaYapilari/ForWhile.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+for index in 1...3{
+    print("Merhaba")
+}
+
+for i in stride(from:10,through:20,by:2){
+    print(i)
+}
+
+for i in stride(from: 20, through: 10, by: -2){
+    print(i)
+}
+
+var sayac = 1
+
+while sayac < 3{
+    print(sayac)
+    sayac+=1
+}
