cfae180a362df66091348b1fe1170b6b287c8aae
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-27 22:16:50 +0300

    feat(ClassAndStructure): add Matematik and Maas classes with functionality
    * Introduced Matematik class for basic arithmetic operations
    * Added Maas class to calculate weekly salary with bonus adjustments

--- 4-NesneTabanliProgramlama/ClassAndStructure.playground/Contents.swift
+++ 4-NesneTabanliProgramlama/ClassAndStructure.playground/Contents.swift
@@ -152,3 +152,44 @@ class Ornek{
 var nesne = Ornek()
 
 print(nesne.no)
+
+
+class Matematik {
+    var x = 10
+    var y = 20
+    
+    var topla : Int{
+        get{
+            return x + y
+        }
+    }
+}
+
+var nesne2 = Matematik()
+
+print(nesne2.topla)
+
+class Maas{
+    var maas = 10000.0
+    var bonus = 1.10
+    
+    var haftalikMaasHesaplama: Double{
+        get{
+            return (maas * bonus)/52
+        }
+        
+        set(yeniHaftalikMaas){
+            self.maas = yeniHaftalikMaas * 52
+        }
+    }
+}
+
+
+var m = Maas()
+
+print(m.haftalikMaasHesaplama)
+m.haftalikMaasHesaplama = 600
+
+print(m.haftalikMaasHesaplama)
+
+print(m.maas)
